9aa2b75c04f68c44e4454351f0788c0d1d2bbb60
rahulnatarajan <rahul_natarajan@hotmail.com>
2018-03-14 11:21:02 +0800

    Modified the Import data script

--- roles/Impala/files/importdata.sh
+++ roles/Impala/files/importdata.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-one=2017-10-31
-two=mv_mco_account
+timekey=$1
+tablename=$2
 
-datadir=/mnt/mcodemo/data/$one
+datadir=/mnt/mcodemo/data/$timekey
 primary_table_script_dir=/mnt/mcodemo/data/hive/primary
-stg2prim=/mnt/mcodemo/data/hive/stg2prim
 staging_table_script_dir=/mnt/mcodemo/data/hive/staging
+stg2prim=/mnt/mcodemo/data/hive/stg2prim
 hadoop_datasets_dir=/mcodemo/datasets
 
 # Check if data directory exists
@@ -16,7 +16,7 @@ fi
 
 # Download from s3
 echo "Downloading dataset from s3..."
-aws s3 cp s3://mcodemo/datasets/$one/$two.csv $datadir/$two.csv
+aws s3 cp s3://mcodemo/datasets/$timekey/$tablename.csv $datadir/$timekey/$tablename.csv
 
 # Check if datasets directory exists
 echo "Ensuring data directory exists in hdfs..."
@@ -26,51 +26,51 @@ if [ $? != 0 ]; then
 fi
 
 # Ensure that directory for the timekey is created
-hadoop fs -mkdir -p $hadoop_datasets_dir/$one
+hadoop fs -mkdir -p $hadoop_datasets_dir/$timekey
 
 # Import to hdfs
 echo "Importing data set to hdfs..."
-hadoop fs -put $datadir/$two.csv $hadoop_datasets_dir/$one/$two.csv
+hadoop fs -put $datadir/$timekey/$tablename.csv $hadoop_datasets_dir/$timekey/$tablename.csv
 
 
 # Load data to table
-if [ "$two" == "mco_entity_bookcode_mapping" ]; then
+if [ "$tablename" == "mco_entity_bookcode_mapping" ]; then
   # Special handling for tables not needing staging
   echo "Downloading create table script from s3..."
-  aws s3 cp s3://mcodemo/hive/primary/$two.sql $primary_table_script_dir/$two.sql
+  aws s3 cp s3://mcodemo/hive/primary/$tablename.sql $primary_table_script_dir/$tablename.sql
 
-  echo "Dropping table $two.."
-  hive -e "drop table $two;"
+  echo "Dropping table $tablename.."
+  hive -e "drop table $tablename;"
 
-  echo "Recreating table $two.."
-  hive -f $primary_table_script_dir/$two.sql
+  echo "Recreating table $tablename.."
+  hive -f $primary_table_script_dir/$tablename.sql
 
-  echo "Loading CSV for $two"
-  hive -e "load data inpath '$hadoop_datasets_dir/$one/$two.csv' INTO TABLE $two"
+  echo "Loading CSV for $tablename"
+  hive -e "load data inpath '$hadoop_datasets_dir/$timekey/$tablename.csv' INTO TABLE $tablename"
 else
-  echo "Downloading create table staging ${two}_stg from s3..."
-  aws s3 cp s3://mcodemo/hive/staging/${two}_stg.sql $staging_table_script_dir/${two}_stg.sql
+  echo "Downloading create table staging ${tablename}_stg from s3..."
+  aws s3 cp s3://mcodemo/hive/staging/${tablename}_stg.sql $staging_table_script_dir/${tablename}_stg.sql
 
-  echo "Dropping table ${two}_staging.."
-  hive -e "drop table ${two}_staging;"
+  echo "Dropping table ${tablename}_staging.."
+  hive -e "drop table ${tablename}_staging;"
 
-  echo "Recreating table ${two}_staging.."
-  hive -f $staging_table_script_dir/${two}_stg.sql
+  echo "Recreating table ${tablename}_staging.."
+  hive -f $staging_table_script_dir/${tablename}_stg.sql
 
-  echo "Loading CSV for ${two}_staging"
-  hive -e "load data inpath '$hadoop_datasets_dir/$one/$two.csv' INTO TABLE ${two}_staging"
+  echo "Loading CSV for ${tablename}_staging"
+  hive -e "load data inpath '$hadoop_datasets_dir/$timekey/$tablename.csv' INTO TABLE ${tablename}_staging"
 
   echo "Downloading create table script from s3..."
-  aws s3 cp s3://mcodemo/hive/primary/$two.sql $primary_table_script_dir/$two.sql
+  aws s3 cp s3://mcodemo/hive/primary/$tablename.sql $primary_table_script_dir/$tablename.sql
 
-  echo "Dropping table $two.."
-  hive -e "drop table $two;"
+  echo "Dropping table $tablename.."
+  hive -e "drop table $tablename;"
 
-  echo "Recreating table $two.."
-  hive -f $primary_table_script_dir/$two.sql
+  echo "Recreating table $tablename.."
+  hive -f $primary_table_script_dir/$tablename.sql
 
   echo "Downloading staging to primary migration script"
-  aws s3 cp s3://mcodemo/hive/stg2primary/${two}_stg2prim.hql $stg2prim/${two}.hql
-
-  hive -f $stg2prim/${two}.hql
+  aws s3 cp s3://mcodemo/hive/stg2primary/${tablename}_stg2prim.hql $stg2prim/${tablename}.hql
+  
+  hive -f $stg2prim/${tablename}.hql 
 fi
