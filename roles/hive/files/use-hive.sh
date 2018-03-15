7815034f4f6a2d1d6807be61becce09fe3dc59d8
rahulnatarajan <rahul_natarajan@hotmail.com>
2018-03-15 12:44:29 +0800

    Hive Update

--- roles/hive/files/use-hive.sh
+++ roles/hive/files/use-hive.sh
@@ -3,12 +3,12 @@
 timekey=$1
 scriptName=$2
 
-echo "Start: $(date)" > ${scriptName}_${timekey}_stat.out
+echo "Start: $(date)" > ${scriptName}_${timekey}_hive_stat.txt
 
 aws s3 cp s3://mcodemo/mdscripts/$scriptName.hql $scriptName.hql
 hive -f $scriptName.hql > ${scriptName}_hive_out.csv
-aws s3 cp ${scriptName}_hive_out.csv s3://mcodemo/md/$timekey/hive/$scriptName.csv
+aws s3 cp ${scriptName}_hive_out.csv s3://mcodemo/md/$timekey/hive/$scriptName_hive_out.csv
 
-echo "End: $(date)" >> ${scriptName}_${timekey}_stat.out
+echo "End: $(date)" >> ${scriptName}_${timekey}_hive_stat.txt
 
-aws s3 cp ${scriptName}_${timekey}_stat.out s3://mcodemo/md/$timekey/stats/${scriptName}_${timekey}_stat.out
+aws s3 cp ${scriptName}_${timekey}_hive_stat.txt s3://mcodemo/md/$timekey/stats/${scriptName}_${timekey}_hive_stat.txt
