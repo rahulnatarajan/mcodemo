4a44c2a0b4512ed9129c3bb00e24a348bbd1475b
rahulnatarajan <rahul_natarajan@hotmail.com>
2018-03-15 12:48:17 +0800

    Script Update

--- roles/presto/files/use-presto.sh
+++ roles/presto/files/use-presto.sh
@@ -3,13 +3,13 @@
 timekey=$1
 scriptName=$2
 
-echo "Start: $(date)" > ${scriptName}_${timekey}_presto.stat
+echo "Start: $(date)" > ${scriptName}_${timekey}_presto_stat.txt
 
 aws s3 cp s3://mcodemo/mdscripts/$scriptName.hql $scriptName.hql
 presto-cli --catalog hive --schema default -f $scriptName.hql > ${scriptName}_presto_out.csv
 aws s3 cp ${scriptName}_presto_out.csv s3://mcodemo/md/$timekey/presto/$scriptName.csv
 
-echo "End: $(date)" >> ${scriptName}_${timekey}_presto.stat
+echo "End: $(date)" >> ${scriptName}_${timekey}_presto_stat.txt
 
-aws s3 cp ${scriptName}_${timekey}_presto.stat s3://mcodemo/md/$timekey/stats/${scriptName}_${timekey}_presto.stat
+aws s3 cp ${scriptName}_${timekey}_presto_stat.txt s3://mcodemo/md/$timekey/stats/${scriptName}_${timekey}_presto_stat.txt
 
--- roles/spark/files/use-spark-sql.sh
+++ roles/spark/files/use-spark-sql.sh
@@ -3,12 +3,12 @@
 timekey=$1
 scriptName=$2
 
-echo "Start: $(date)" > ${scriptName}_${timekey}_sparksql.stat
+echo "Start: $(date)" > ${scriptName}_${timekey}_sparksql_stat.txt
 
 aws s3 cp s3://mcodemo/mdscripts/$scriptName.hql $scriptName.hql
 spark-sql -f $scriptName.hql > ${scriptName}_sparksql_out.csv
 aws s3 cp ${scriptName}_sparksql_out.csv s3://mcodemo/md/$timekey/sparksql/${scriptName}_sparksql_out.csv
 
-echo "End: $(date)" >> ${scriptName}_${timekey}_sparksql.stat
+echo "End: $(date)" >> ${scriptName}_${timekey}_sparksql_stat.txt
 
-aws s3 cp ${scriptName}_${timekey}_stat.out s3://mcodemo/md/$timekey/stats/${scriptName}_${timekey}_sparksql.stat
+aws s3 cp ${scriptName}_${timekey}_sparksql_stat.txt s3://mcodemo/md/$timekey/stats/${scriptName}_${timekey}_sparksql_stat.txt
