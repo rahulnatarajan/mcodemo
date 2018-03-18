cd97e9208689dc2f0c91f185862e1b7d59075f3e
rahulnatarajan <rahul_natarajan@hotmail.com>
2018-03-18 14:02:10 +0800

    updated report scripts

--- roles/hive/files/use-hive.sh
+++ roles/hive/files/use-hive.sh
@@ -7,7 +7,7 @@ echo "Start: $(date)" > ${scriptName}_${timekey}_hive_stat.txt
 
 aws s3 cp s3://mcodemo/mdscripts/$scriptName.hql $scriptName.hql
 hive -f $scriptName.hql > ${scriptName}_hive_out.csv
-aws s3 cp ${scriptName}_hive_out.csv s3://mcodemo/md/$timekey/hive/$scriptName_hive_out.csv
+aws s3 cp ${scriptName}_hive_out.csv s3://mcodemo/md/$timekey/hive/${scriptName}_hive_out.csv
 
 echo "End: $(date)" >> ${scriptName}_${timekey}_hive_stat.txt
 
--- roles/presto/files/use-presto.sh
+++ roles/presto/files/use-presto.sh
@@ -7,7 +7,7 @@ echo "Start: $(date)" > ${scriptName}_${timekey}_presto_stat.txt
 
 aws s3 cp s3://mcodemo/mdscripts/$scriptName.hql $scriptName.hql
 presto-cli --catalog hive --schema default -f $scriptName.hql > ${scriptName}_presto_out.csv
-aws s3 cp ${scriptName}_presto_out.csv s3://mcodemo/md/$timekey/presto/$scriptName.csv
+aws s3 cp ${scriptName}_presto_out.csv s3://mcodemo/md/$timekey/presto/${scriptName}_presto_out.csv
 
 echo "End: $(date)" >> ${scriptName}_${timekey}_presto_stat.txt
 
